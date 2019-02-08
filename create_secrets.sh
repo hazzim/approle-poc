0baed86270233c7e090cc22b2b9f274c43ce6ad5
Nathan Basanese (git.io/v6) <v6@users.noreply.github.com>
2019-02-07 16:51:26 -0800

    // , Add compatibility for KV2 Secrets Engine
    This adds some comments, and tries with the KV1 paths, then with the
    KV2 paths.
    
    Please make sure to modify the policies accordingly, and test it.
    
    Please use Vault Policies for limiting access, because the creator of the secrets should have
    "least privilege."

--- create_secrets.sh
+++ create_secrets.sh
@@ -1,3 +1,5 @@
+  ##  For KV1
+
 curl -sS \
     --header "X-Vault-Token: $VAULT_TOKEN" \
     --request POST \
@@ -9,3 +11,20 @@ curl -sS \
     --request POST \
     --data @payload2.json \
     "${VAULT_ADDR}/v1/secret/onlinestore/pos/password"
+
+
+  ##  For KV2 (Includes data/ in the path after the KV Secret Engine name)
+
+curl -sS \
+    --header "X-Vault-Token: $VAULT_TOKEN" \
+    --request POST \
+    --data @payload1.json \
+    "${VAULT_ADDR}/v1/secret/data/onlinestore/pos/username"
+
+curl -sS \
+    --header "X-Vault-Token: $VAULT_TOKEN" \
+    --request POST \
+    --data @payload2.json \
+    "${VAULT_ADDR}/v1/secret/data/onlinestore/pos/password"
+
+
