66e17c4a5736ec62b02915df2b02e4b53b147ae4
Hazzim Anaya <hazzim.anaya@gmail.com>
2019-02-11 16:23:24 -0600

    Add new approle, one to create user/pass, and the second one to read that user/pass

--- create_approle.sh
+++ create_approle.sh
@@ -9,8 +9,14 @@ echo add new role from JSON file
 curl -sS \
     --header "X-Vault-Token: $VAULT_TOKEN" \
     --request POST \
+    --data @payload4.json \
+    "${VAULT_ADDR}/v1/auth/approle/role/postgre_create" | jq
+
+curl -sS \
+    --header "X-Vault-Token: $VAULT_TOKEN" \
+    --request POST \
     --data @payload.json \
-    "${VAULT_ADDR}/v1/auth/approle/role/postgre" | jq
+    "${VAULT_ADDR}/v1/auth/approle/role/postgre_read" | jq
 
 echo Reading approle 
 curl -sS \
